bc9ddfd220bab4ea56e84011d8bca4c1e3132c39
Alexandru Munteanu <alexandru.munteanu@thinslices.com>
2017-05-25 12:12:51 +0300

    add tenics-bot serverless api service

---
+++ tenics-bot/books.js
@@ -0,0 +1,7 @@
+export const create = async (event, context, callback) => {
+  callback(null, 'create books')
+}
+
+export const update = async (event, context, callback) => {
+  callback(null, 'update books')
+}
