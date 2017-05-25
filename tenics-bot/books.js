6663605503013f9329d46a595220cc96386016d3
Alexandru Munteanu <alexandru.munteanu@thinslices.com>
2017-05-25 16:46:42 +0300

    add book to room

--- tenics-bot/books.js
+++ tenics-bot/books.js
@@ -2,7 +2,7 @@ import fetch from 'node-fetch'
 import * as db from './libs/dynamo-lib'
 import { success, failure } from './libs/response-lib'
 
-const createFetchURL = (bookTitle, lang = 'en') =>
+const createGoogleFetchURL = (bookTitle, lang = 'en') =>
   encodeURI(`https://www.googleapis.com/books/v1/volumes?q=${bookTitle}&langRestrict=${lang}&orderBy=relevance&maxResults=2`)
 
 const mapBookItem = (book) => ({
@@ -14,10 +14,37 @@ const mapBookItem = (book) => ({
   "pages": book.volumeInfo.pageCount
 })
 
+export const findBook = async (event, context, callback) => {
+  const { bookTitle, roomId } = JSON.parse(event.body)
+
+  const result = await fetch(createGoogleFetchURL(bookTitle))
+  const json = await result.json()
+  const book = mapBookItem(json.items[0])
+
+  const queryParams = {
+    TableName: 'tenics-books',
+    KeyConditionExpression: "bookId = :bookId",
+    ExpressionAttributeValues: {
+      ":bookId": book.bookId
+    }
+  }
+
+  try {
+    const { Count } = await db.call(db.DYNAMO_ACTIONS.query, queryParams)
+    console.log('db book', Count)
+    if (!Count) {
+      console.log('no book found, need to create it')
+    }
+  } catch (e) {
+    console.log('err', e)
+    callback(null, failure({ status: false }))
+  }
+}
+
 export const create = async (event, context, callback) => {
   const { bookTitle } = JSON.parse(event.body)
 
-  const result = await fetch(createFetchURL(bookTitle))
+  const result = await fetch(createGoogleFetchURL(bookTitle))
   const json = await result.json()
   
   const book = mapBookItem(json.items[0])
@@ -27,7 +54,7 @@ export const create = async (event, context, callback) => {
   }
 
   try {
-    await db.call('put', params)
+    await db.call(db.DYNAMO_ACTIONS.put, params)
     callback(null, success(book))
   } catch (e) {
     callback(null, failure({ status: false }))
--- tenics-bot/libs/dynamo-lib.js
+++ tenics-bot/libs/dynamo-lib.js
@@ -6,3 +6,15 @@ export function call(action, params) {
   const db = new AWS.DynamoDB.DocumentClient()
   return db[action](params).promise()
 }
+
+export function createSet(item) {
+  const db = new AWS.DynamoDB.DocumentClient()
+  return db.createSet([JSON.stringify(item)])
+}
+
+export const DYNAMO_ACTIONS = {
+  get: 'get',
+  put: 'put',
+  query: 'query',
+  update: 'update'
+}
