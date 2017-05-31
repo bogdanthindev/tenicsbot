dfae971c4156d83ae995e0d2fc756ee7593068e6
Alexandru Munteanu <alexandru.munteanu@thinslices.com>
2017-05-31 11:52:53 +0300

    remove google API key from google books

--- api/googleBooks.js
+++ api/googleBooks.js
@@ -1,9 +1,7 @@
 const books = require('google-books-search')
-const { API_KEY } = require('../config')
 const Promise = require('bluebird')
 
 const options = {
-  key: API_KEY,
   limit: 1,
   type: 'books',
   order: 'relevance',
