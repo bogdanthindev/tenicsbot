2eaecb03e6be6447e6e9a963bec1ff22bdea922a
Alexandru Munteanu <alexandru.munteanu@thinslices.com>
2017-06-02 10:02:09 +0300

    add slack client package

--- server.js
+++ server.js
@@ -10,35 +10,35 @@ const connectToMongoDB = (cb) => {
   const MongoClient = require('mongodb').MongoClient
 
   MongoClient.connect(mongoString, function mongoConnect(err, conn) {
-      if (err) { return cb(err) }
+    if (err) { return cb(err) }
 
-      global.mongo = conn
-      return cb()
+    global.mongo = conn
+    return cb()
   })
 }
 
 const startServer = () => {
-    const app = express()
+  const app = express()
 
-    app.use(bodyParser.json())
-    app.use(bodyParser.urlencoded({ extended: true }))
+  app.use(bodyParser.json())
+  app.use(bodyParser.urlencoded({ extended: true }))
 
-    /* Handling all messenges */
-    app.post('/webhook', apiAiController)
-    app.post('/interactive', interactiveController)
-
-
-    const agenda = new Agenda({db: {address: mongoString}})
-    agenda.define('send reminders', reminderController.sendReminders);
-    agenda.on('ready', () => {
-        agenda.every('30 seconds', 'send reminders')
-        agenda.start()
-    })
+  /* Handling all messenges */
+  app.post('/webhook', apiAiController)
+  app.post('/interactive', interactiveController)
 
 
-    const server = app.listen(5000, () => {
-        console.log('Express server listening on port %d in %s mode', server.address().port, app.settings.env)
-    })
+  const agenda = new Agenda({db: {address: mongoString}})
+  agenda.define('send reminders', reminderController.sendReminders);
+  agenda.on('ready', () => {
+    agenda.every('30 seconds', 'send reminders')
+    agenda.start()
+  })
+
+
+  const server = app.listen(5000, () => {
+    console.log('Express server listening on port %d in %s mode', server.address().port, app.settings.env)
+  })
 }
 
 connectToMongoDB(startServer);
