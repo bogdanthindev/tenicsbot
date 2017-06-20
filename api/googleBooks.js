5da86a7bc5a2923a7da0cce5fef9957ac144ff28
Alexandru Munteanu <alexandru.munteanu@thinslices.com>
2017-06-20 11:58:57 +0300

    return a promise

--- api/googleBooks.js
+++ api/googleBooks.js
@@ -1,23 +1,28 @@
 const fetch = require('node-fetch')
 
 const createGoogleFetchURL = (searchItem, lang = 'en') =>
-  encodeURI(`https://www.googleapis.com/books/v1/volumes?q=${searchItem}&langRestrict=${lang}&orderBy=relevance&maxResults=3`)
+  encodeURI(
+    `https://www.googleapis.com/books/v1/volumes?q=${searchItem}&langRestrict=${lang}&orderBy=relevance&maxResults=3`
+  )
 
-const mapBookItem = (book) => ({
-  "bookId": book.id,
-  "title": book.volumeInfo.title,
-  "author": book.volumeInfo.authors[0],
-  "thumbnail": book.volumeInfo.imageLinks.thumbnail,
-  "description": book.volumeInfo.description,
-  "pages": book.volumeInfo.pageCount
+const mapBookItem = book => ({
+  bookId: book.id,
+  title: book.volumeInfo.title,
+  author: book.volumeInfo.authors[0],
+  thumbnail: book.volumeInfo.imageLinks.thumbnail,
+  description: book.volumeInfo.description,
+  pages: book.volumeInfo.pageCount
 })
 
 const searchBook = (bookTitle, author) => {
   const fetchUrl = createGoogleFetchURL(`${bookTitle} ${author}`.trim())
   console.log('search url', fetchUrl)
-  fetch(createGoogleFetchURL(`${bookTitle} ${author}`.trim()))
+  return fetch(createGoogleFetchURL(`${bookTitle} ${author}`.trim()))
     .then(r => r.json())
-    .then(result => mapBookItem(result.items[0]))
+    .then(result => {
+      console.log('book results', JSON.stringify(result, null, 2))
+      return mapBookItem(result.items[0])
+    })
 }
 
 module.exports = {
